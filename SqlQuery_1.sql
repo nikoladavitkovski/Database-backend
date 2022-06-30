375124c8a3225f5aa791592a774cd6a597410f07
nikoladavitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2022-06-30 14:16:14 +0200

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE wireable
+CREATE TABLE BackEndProject(
+	id INT NOT NULL PRIMARY KEY,
+	username nvarchar(50) NOT NULL,
+	password nvarchar(50) NOT NULL,
+	userid INT NOT NULL PRIMARY KEY,
+	primary key(id),
+	)
+
+CREATE TABLE BackEndProject(
+	id INT NOT NULL PRIMARY KEY,
+	username nvarchar(50) NOT NULL,
+	password nvarchar(50) NOT NULL,
+	userid INT NOT NULL PRIMARY KEY,
+	primary key(id),
+	)
+
+BEGIN
+
+RETURN username
+RETURN password
+RETURN userid
+
+END
